f1f7a373b4af3411da296e5bf0ba335e08a8a799
137010393@qq.com <jiayouxl2012>
2018-11-29 15:54:36 +0800

    第一次提交

--- www/app.py
+++ www/app.py
@@ -8,6 +8,20 @@ def index():
 @app.route("/user/<name>",methods=['POST','GET'])
 def user(name):
     return "<h1>Hello,%s!</h1>" %name
+@app.route("/login",methods=['GET'])
+def form():
+    return render_template("login.html")
+
+@app.route("/login",methods=['POST'])
+def login():
+    # 需要从request对象读取表单内容：
+    username = request.form['username']
+    password = request.form['password']
+    if username=='admin' and password=='123456':
+
+     return render_template("index.html",message="欢迎"+username);
+    else:
+     return render_template("login.html",message="密码或者账号错误",username = username)
 if __name__ == '__main__':
     app.run(debug=True)
 
